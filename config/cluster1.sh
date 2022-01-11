c1e26514f339832ab91ed02a9468fbcb0a3450f2
JZ <ee.jzabelin@gmail.com>
2022-01-11 19:25:25 +0000

    modified redis cluster

--- config/cluster1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-redis-server /usr/local/etc/redis/redis_1.conf
-redis-server /usr/local/etc/redis/redis_11.conf
--- config/cluster3.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-redis-server /usr/local/etc/redis/redis_3.conf
-redis-server /usr/local/etc/redis/redis_13.conf
